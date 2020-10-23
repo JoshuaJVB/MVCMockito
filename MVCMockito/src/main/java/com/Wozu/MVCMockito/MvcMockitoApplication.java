a259d240c5c1be013e44e0a834defc8936e3ef62
Joshua Van Baalen <joshuavanbaalen@gmail.com>
2020-10-23 07:22:06 -0600

    Uploading base

---
+++ MVCMockito/src/main/java/com/Wozu/MVCMockito/MvcMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.Wozu.MVCMockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcMockitoApplication.class, args);
+	}
+
+}
---
+++ MVCMockito/src/test/java/com/Wozu/MVCMockito/MvcMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Wozu.MVCMockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
